ed66a9142bede8119463be586ac4d3fccaec5732
Sophie Lewis <sophie.lewis@lexicondigital.com.au>
2021-12-21 14:48:16 +1100

    login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#94e0a6',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+      background: {
+        default: '#fafafa',
+      },
+    },
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+  <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
