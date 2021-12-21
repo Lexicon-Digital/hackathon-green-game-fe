8a04a7fb7b5d72928a82b64ad5d2e1961ac76425
Sophie Lewis <sophie@Sophies-MBP.lexicondigital.com.au>
2021-12-21 12:48:54 +1100

    rough signup page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,24 @@ import Button from '@mui/material/Button';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Button variant="contained">Hello World</Button>
+      <h1 className={styles.heading}>Green Game</h1>
+      <p className={styles.headingMessage}>Welcome to the beginning of your journey to be a more environmentally friendly shopper.</p>
+      <form className={styles.signUp}>
+  <label>
+    <input type="text" name="name" className={styles.inputField} placeholder="Name"/>
+  </label>
+  <label>
+    <input type="text" name="email" className={styles.inputField}placeholder="Email"/>
+  </label>
+  <label>
+    <input type="text" name="password" className={styles.inputField}placeholder="Password"/>
+  </label>
+  <label>
+    <input type="text" name="confirmpassword" className={styles.inputField}placeholder="Confirm Password"/>
+  </label>
+  <Button variant="contained">Submit</Button>
+</form>
+      
     </div>
   )
 }
