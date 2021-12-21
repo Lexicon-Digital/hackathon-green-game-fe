063abda53dfdbc4662136d26ed3f735f00d9b502
Steph Vallance <44055497+steph603@users.noreply.github.com>
2021-12-21 12:27:13 +1100

    Navbar component & styles

---
+++ components/Navbar/index.js
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+import styles from "./styles.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
