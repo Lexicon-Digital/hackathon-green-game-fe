adbfa7c083ad98f72e13f43b17a1015b80a10bd4
Steph Vallance <44055497+steph603@users.noreply.github.com>
2021-12-21 16:24:10 +1100

    Resolved merged conflict

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,54 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Button from "@mui/material/Button";
-
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <ResponsiveAppBar />
-
-import Navbar from "../components/Navbar";
-import Shoppinglist from "../components/Shoppinglist";
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>Green Game</h1>
-      <p className={styles.headingMessage}>Welcome to the beginning of your journey to be a more environmentally friendly shopper.</p>
-      <form className={styles.signUp}>
-  <label>
-    <input type="text" name="name" className={styles.inputField} placeholder="Name"/>
-  </label>
-  <label>
-    <input type="text" name="email" className={styles.inputField}placeholder="Email"/>
-  </label>
-  <label>
-    <input type="text" name="password" className={styles.inputField}placeholder="Password"/>
-  </label>
-  <label>
-    <input type="text" name="confirmpassword" className={styles.inputField}placeholder="Confirm Password"/>
-  </label>
-  <Button variant="contained">Submit</Button>
-</form>
-      <Navbar />
-      <Shoppinglist />
-
-    </div>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Green Game</h1>
+        <p className={styles.headingMessage}>
+          Welcome to the beginning of your journey to be a more environmentally
+          friendly shopper.
+        </p>
+        <form className={styles.signUp}>
+          <label>
+            <input
+              type="text"
+              name="name"
+              className={styles.inputField}
+              placeholder="Name"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="email"
+              className={styles.inputField}
+              placeholder="Email"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="password"
+              className={styles.inputField}
+              placeholder="Password"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="confirmpassword"
+              className={styles.inputField}
+              placeholder="Confirm Password"
+            />
+          </label>
+          <Button variant="contained">Submit</Button>
+        </form>
+      </div>
+    </>
   );
 }
---
+++ pages/shoppinglist.js
@@ -0,0 +1,5 @@
+import Shoppinglist from "../components/Shoppinglist";
+
+export default function ShoppingList() {
+  return <Shoppinglist />;
+}
